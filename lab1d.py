d0670b4cf9e43ae1344e1d44c4d05be85adc3e31
Tania Chaudhary <tchaudhary3@myseneca.ca>
2025-01-17 13:19:53 -0500

    Added lab1d.py

---
+++ lab1d.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+'''Description: This program demonstrates string and integer concatenation.'''
+
+# Assign values to objects
+x = 10
+y = 2
+z = 5
+
+# Perform the calculation and print the result
+print(str(x) + ' + ' + str(y) + ' * ' + str(z) + ' = '  + str(x + y  * z))
