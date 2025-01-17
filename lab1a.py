307450dd66da3af68943f7766e629e9d6baf943c
Tania Chaudhary <tchaudhary3@myseneca.ca>
2025-01-17 12:51:10 -0500

    Added lab1a

---
+++ lab1a.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+'''Description: This program will output "hello world" to the screen.'''
+
+# Any line that starts with a "#" is a comment and is ignored by Python.
+# The very first line (shebang) tells the system to use python3.
+
+print('Hello world')
