8d9b6e35d505fb611f1209435691ab76c1470103
Tania Chaudhary <tchaudhary3@myseneca.ca>
2025-01-17 13:04:28 -0500

    Added lab1c.py

---
+++ lab1c.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+'''Description: This program demonstrates string and integer concatenation.'''
+
+# Assign values to objects
+name = 'Isaac'
+age = 72
+
+# Print the message using the objects
+print(name + ' is ' + str(age) + ' years old!')
