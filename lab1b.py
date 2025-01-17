233e4263e0b6d30c9e05f5b2c54de1828478764b
Tania Chaudhary <tchaudhary3@myseneca.ca>
2025-01-17 13:01:11 -0500

    Added lab1b.py

---
+++ lab1b.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+'''Description: This program outputs a message with a name.'''
+
+# Assign a string to the name object
+name = 'Isaac'
+
+# Print the message using the name object
+print('How old are you ' + name + '?')
